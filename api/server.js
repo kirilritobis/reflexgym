9e9e18984698af40224182271bd947a541ca3460
kirilritobis <kirilritobis@gmail.com>
2022-04-06 16:00:57 +0300

    changed backend port to 8001

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8001;
 
 const server = http.createServer(app);
 
