346fb275e2d7c80360afc62e9d132c7fbe23f282
kirilritobis <kirilritobis@gmail.com>
2022-04-14 18:18:27 +0300

    Return access token on code confirmation

---
+++ api/helpers/tokenGenerator.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+function generateTokens (user) {
+    try {
+        console.log(user, process.env.ACCESS_TOKEN_SECRET, process.env.REFRESH_TOKEN_SECRET)
+        const accessToken = jwt.sign( { email: user.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60s'})
+        const refreshToken = jwt.sign( { email: user.email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d'})
+
+        return { accessToken: accessToken, refreshToken: refreshToken }
+    } catch (err) {
+        logger.error('%o', err)
+        // return errorhandler.sendError(err, req, res)
+        return err
+    }
+}
+
+module.exports = {
+    generateTokens
+}
