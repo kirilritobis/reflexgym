573e19f5ddc43bc4ab408b71d3ab05bc63a49343
Kristiyan Peev <kristiyanpp@gmail.com>
2022-04-25 22:11:24 +0200

    added table in web

---
+++ web/src/components/UsersAll/UserDetailsExpanded.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+interface UsersExpandedProps {}
+
+const UserDetailsExpanded: FunctionComponent<UsersExpandedProps> = (props) => {
+  return <div>asd</div>;
+};
+
+export default UserDetailsExpanded;
---
+++ web/src/components/UsersAll/UserRow.tsx
@@ -0,0 +1,62 @@
+import React, { FunctionComponent, useState } from "react";
+
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import UserDetailsExpanded from "./UserDetailsExpanded";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+
+interface UserRowProps {
+  row: any;
+}
+
+const UserRow: FunctionComponent<UserRowProps> = (props) => {
+  const { row } = props;
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <React.Fragment>
+      <TableRow
+      // sx={{ "& > *": { borderBottom: "unset" } }}
+      // sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+      >
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? (
+              <KeyboardArrowUpIcon fontSize="small" />
+            ) : (
+              <KeyboardArrowDownIcon fontSize="small" />
+            )}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.calories}</TableCell>
+        <TableCell align="right">{row.fat}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <UserDetailsExpanded />
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+};
+
+export default UserRow;
